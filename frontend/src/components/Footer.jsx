f192d730b99168c940e38a9b9f29c77e2a176ce8
Miguel <carrerasaezmiguel@gmail.com>
2026-05-04 20:18:07 -0400

    delete reliquia

--- frontend/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Footer() {
-  return (
-    <footer style={{ padding: '20px', textAlign: 'center', backgroundColor: '#333', color: 'white', marginTop: '40px' }}>
-      <p>© 2026 Iconbototos. Todos los derechos reservados.</p>
-    </footer>
-  );
-}
