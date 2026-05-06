d244eb2ae937e585438b10c99f44382d70f99bcd
Miguel <carrerasaezmiguel@gmail.com>
2026-05-06 01:24:59 -0400

    asd

--- frontend/src/components/Catalogo.jsx
+++ frontend/src/components/Catalogo.jsx
@@ -25,9 +25,6 @@ export default function Catalogo({ agregarAlCarrito }) {
 
   return (
       <section id="catalogo" className="seccion-catalogo">
-      <h2 style={{ textAlign: 'center', marginBottom: '40px', fontSize: '2.5rem', textTransform: 'uppercase' }}>
-      Nuestro Catálogo 
-      </h2>
       
       {cargando ? (
         <div style={{ textAlign: 'center', padding: '50px' }}>
