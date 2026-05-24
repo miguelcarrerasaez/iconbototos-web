071154a6d42c61f9e981d2656a61be379fa1fc11
Miguel <carrerasaezmiguel@gmail.com>
2026-05-24 00:41:41 -0400

    video out

--- frontend/src/components/VideoBanner.jsx
+++ frontend/src/components/VideoBanner.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+{/*import React from 'react';
 
 export default function VideoBanner() {
   return (
@@ -28,4 +28,4 @@ export default function VideoBanner() {
       />
     </section>
   );
-}
+}  
