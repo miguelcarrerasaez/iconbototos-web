ea3047614acf7ab4329ee700c39f845bbd49c89c
Miguel <carrerasaezmiguel@gmail.com>
2026-08-07 21:28:04 -0400

    10

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -22,20 +22,20 @@ export default function Navbar({ carrito, setIsCartOpen }) {
        {/* 2. GRUPO DERECHA: Login, Buscar, Carrito y Menú */}
        <div style={{ display: 'flex', gap: '5px', alignItems: 'center' }}>
          <Link to="/admin" style={{ display: 'flex', alignItems: 'center' }}>
-           <img src="/img/MenúCuenta.png" alt="Cuenta" style={{ width: '40px', height: '40px' }} />
+           <img src="/img/MenúCuenta.svg" alt="Cuenta" style={{ width: '40px', height: '40px' }} />
          </Link>
 
          <button style={{ background: 'none', border: 'none', cursor: 'pointer', padding: 0 }}>
-           <img src="/img/MenúBuscar.png" alt="Buscar" style={{ width: '40px', height: '40px' }} />
+           <img src="/img/MenúBuscar.svg" alt="Buscar" style={{ width: '40px', height: '40px' }} />
          </button>
 
          <button onClick={() => setIsCartOpen(true)} style={{ background: 'none', border: 'none', cursor: 'pointer', position: 'relative', padding: 0 }}>
-           <img src="/img/MenúCarrito.png" alt="Carrito" style={{ width: '40px', height: '40px' }} />
+           <img src="/img/MenúCarrito.svg" alt="Carrito" style={{ width: '40px', height: '40px' }} />
            {totalItems > 0 && <span style={{ position: 'absolute', top: '-5px', right: '-5px', fontSize: '10px', background: '#ff48b0', color: 'white', borderRadius: '50%', padding: '2px 5px' }}>{totalItems}</span>}
          </button>
 
          <button onClick={() => setMenuOpen(!menuOpen)} style={{ background: 'none', border: 'none', cursor: 'pointer', padding: 0 }}>
-           <img src="/img/MenúHamburguesa.png" alt="Menú" style={{ width: '40px', height: '40px' }} />
+           <img src="/img/MenúHamburguesa.svg" alt="Menú" style={{ width: '40px', height: '40px' }} />
          </button>
        </div>
 
