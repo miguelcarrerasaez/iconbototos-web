99ea77d467c35beaf5ab7e6a11dfaff88c6c274c
Miguel <carrerasaezmiguel@gmail.com>
2026-08-30 20:21:43 -0400

    producto2

--- frontend/src/data/productos.js
+++ frontend/src/data/productos.js
@@ -6,11 +6,33 @@ export const productos = [
     autor: "Monserrat Mella González",
     precio: "$12.000",
     stock: true,
-    imagenes: ["/img/domingo.png", "/img/domingo_2.jpg", "/img/domingo_3.jpg"],
+    imagenes: [
+      "/img/domingo.jpg", 
+      "/img/domingo_2.png", 
+      "/img/domingo_3.jpg"
+    ],
     descripcion: {
       sinopsis: "Domingo explora a través de viñetas en completo silencio, las distintas emociones que experimenta una persona el día domingo, previo a comenzar la semana laboral en un trabajo poco satisfactorio. La calma al regar las plantas, hacer desayuno, se contrapone con la aplastante realidad del día lunes y del trabajo formal en un contexto capitalista.",
       bio: "Monserrat Mella González es Ilustradora y Diseñadora Gráfica.",
       fichaTecnica: "Primera edición / 30 copias / 12 páginas / 21.5 x 28.5 cm / Publication date: 2024"
+    }
+  },
+  {
+    id: "2",
+    titulo: "HAY FLORES QUE CRECEN SOBRE BICICLETAS FANTASMA",
+    categoria: "Fanzine",
+    autor: "Carolina Benalcázar",
+    precio: "$12.000",
+    stock: true,
+    imagenes: [
+      "/img/hay_flores_1.jpg", // Recuerda guardar tu foto exportada con este nombre en public/img/
+      "/img/hay_flores_2.jpg", 
+      "/img/hay_flores_3.jpg"
+    ],
+    descripcion: {
+      sinopsis: "Fanzine de autora, por Carolina Benalcázar, que reúne sus textos y serigrafía.",
+      bio: "Carolina Benalcázar es ilustradora y editora. Desde Quito, Ecuador, forma parte de Invasión 1989.",
+      fichaTecnica: "Primera edición / 30 copias por color / 12 páginas / 21.5 x 28.5 cm / Publication date: 2024"
     }
   }
 ];
