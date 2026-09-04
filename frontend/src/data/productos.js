b1969b00eb43cb0ee52b0a2e270831effe9d400f
Miguel <carrerasaezmiguel@gmail.com>
2026-09-04 17:10:48 -0400

    prueba_letras

--- frontend/src/data/productos.js
+++ frontend/src/data/productos.js
@@ -30,9 +30,11 @@ export const productos = [
       "/img/hay_flores3.png"
     ],
     descripcion: {
-      sinopsis: "Fanzine sobre perros, bicicletas y la música que nos acompaña.",
-      bio: "Carolina Benalcázar es ilustradora y editora. Reside en Quito, Ecuador. Forma parte de Recodo Press. ",
-      fichaTecnica: "Primera edición / 50 copias / páginas por definir / medidas por definir / Publication date: 2014"
+      sinopsis: "Fanzine sobre perros, bicicletas y la música que nos acompaña."
+      "Carolina Benalcázar es ilustradora y editora. Reside en Quito, Ecuador. Forma parte de Recodo Press."
+      "Primera edición / 50 copias / páginas por definir / medidas por definir / Publication date: 2014"
+      bio:  
+      fichaTecnica: 
     }
   },
   {
